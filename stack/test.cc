4c0318fe74c666b6b19b8af51f045ff4826bfe8e
Adam Morley <adam.morley@gmail.com>
2019-09-24 14:41:55 +0200

    updates

--- stack/test.cc
+++ stack/test.cc
@@ -3,14 +3,19 @@
 
 #include "stack.h"
 
+const int SIZE = 12;
+
 void test() {
-    stack<int> s1(17);
-    s1.push(1);
-    s1.push(2);
-    assert(s1.size() == 17);
-    assert(s1.count() == 2);
-    assert(!s1.full());
-    assert(2 == s1.pop());
+    stack<int> s(SIZE);
+    assert(s.empty());
+    s.push(1);
+    s.push(2);
+    assert(s.size() == SIZE);
+    assert(s.count() == 2);
+    assert(!s.full());
+    assert(2 == s.pop());
+    assert(1 == s.pop());
+    assert(s.empty());
 }
 
 int main() {
